6a8d415ce3c7b7e380ce28fbbc8951f373085574
Grayson Wu <58651725+Graysonnwu@users.noreply.github.com>
2022-06-06 23:08:11 +0800

    增加了总赞数、播放数、阅读数的统计

--- run-plus.sh
+++ run-plus.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 python3 bilibili-plus.py >> ./data/data-plus.txt
 echo "date,follower,likes,video_view,article_view" > ./data/data-plus.csv
 tac ./data/data-plus.txt >> ./data/data-plus.csv
