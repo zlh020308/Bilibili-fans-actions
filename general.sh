795a0cf099017ab6d4bcfbbc7f199b9bfa947add
Grayson Wu <58651725+Graysonnwu@users.noreply.github.com>
2022-10-01 12:56:11 +0800

    Delete general.sh

--- general.sh
+++
@@ -1,5 +0,0 @@
-while IFS= read -r line; do
-    python3 general.py $line >> ./txt/$line.txt
-    echo "date,follower,likes,video_view,article_view" > ./csv/$line.csv
-    tac ./txt/$line.txt >> ./csv/$line.csv
-done < uid.txt
